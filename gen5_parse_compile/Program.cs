2d40d4c0022ba3f2896a11ef6d5d62330cc9fb96
PhoenixBound <phoenixbound.github@gmail.com>
2017-06-08 18:02:54 -0500

    Rewrite a bunch of things to be cleaner

--- gen5_parse_compile/Program.cs
+++ gen5_parse_compile/Program.cs
@@ -10,12 +10,12 @@ namespace gen5_parse_compile
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             /* if (args.Length == 0)
                    ; // show compiler help
                if (args.Length == 1)
-                   ; // set single arg to filename, for parsing   
+                   ; // set single arg to filename, for parsing
              */
             
             // **************
@@ -26,60 +26,64 @@ namespace gen5_parse_compile
             // TODO: use an argument for this
             string scriptFile = "6_180.bin";
             // Store script offsets for later
-            List<uint> offsets = new List<uint>();
 
-            if (File.Exists(scriptFile))
+            // This needs to be replaced with better logic
+            if (!File.Exists(scriptFile))
             {
-                // ********************************************************************
-                // Start by finding all the offsets, and adding them to the offset list
-                // ********************************************************************
-                using (BinaryReader reader = new BinaryReader(File.Open(scriptFile, FileMode.Open)))
-                {
-                    Console.WriteLine("File opened: {0}", scriptFile);
-                    // 0xFD13 is the magic ushort that ends the offset section.
-                    while (offsets.Count == 0 || (offsets[offsets.Count - 1] & 0xFD13) != 0xFD13)
-                    {
-                        offsets.Add(reader.ReadUInt32());
-                        // Debug: write offsets as hex strings
-                        Console.WriteLine("New offset: {0}", offsets[offsets.Count - 1].ToString("X8"));
-                    }
-
-                    // Remove the last offset, the FD13 offset doesn't actually exist
-                    offsets.RemoveAt(offsets.Count - 1);
-
-                    // ************************************************************************
-                    // Alright. We have the offsets, now let's follow them to find the scripts.
-                    // ************************************************************************
-
-                    for (int index = 0; index < offsets.Count; index++)
-                    {
-                        // Reset the position of the stream to 0x0, and seek to the first script offset
-                        // Desired offset = (read position) + (listed offset), where read position is
-                        // whatever offset the stream is at after reading a value.
-                        //
-                        // This should be refactored to be all at once with the offset finding, to be
-                        // honest. It's ugly to find the offset again.
-                        reader.BaseStream.Seek((index + 1) * 4 + offsets[index], SeekOrigin.Begin);
-                        Console.WriteLine("Finding data at offset {0}", reader.BaseStream.Position.ToString("X8"));
-                        ushort command = 0;
-                        while (command != 0x2)
-                        {
-                            command = reader.ReadUInt16();
-                            // TODO: Add argument support, like from a separate function.
-                            // TODO: Make arg count read from a separate file?
-                            // TODO: Add command names
-                        }
-                    }
-                }
-
+                Console.WriteLine("File '{0}' could not be found. Exiting...", scriptFile);
+                return;
             }
-            else
-            {
-                Console.WriteLine("File " + scriptFile + " does not exist. Dang it.");
-            }
+
+            // Let's make some magic
+            ReadScriptsFromHeader(scriptFile);
 
             Console.ReadLine();
 
+            return;
+        }
+
+        static void ReadScriptsFromHeader(string scr)
+        {
+            using (BinaryReader reader = new BinaryReader(File.Open(scr, FileMode.Open)))
+            {
+                for (uint scriptOffset = reader.ReadUInt32(), currentOffset = (uint)reader.BaseStream.Position;;)
+                {
+                    // The 16-bit value 0xFD13 marks the end of the header, so
+                    // no more scripts exist if this is true
+                    if ((ushort)scriptOffset == 0xFD13)
+                        break;
+
+                    // So we found a script to jump to, then.
+                    Console.WriteLine("Offset: {0:X8}", scriptOffset);
+
+                    // Seek forward, and read the script.
+                    reader.BaseStream.Seek(scriptOffset, SeekOrigin.Current);
+                    ReadScript(reader);
+
+                    // After the script's been read, jump back and read the next offset.
+                    reader.BaseStream.Seek(currentOffset, SeekOrigin.Begin);
+
+                    scriptOffset = reader.ReadUInt32();
+                    currentOffset = (uint)reader.BaseStream.Position;
+                }
+            }
+
+            return;
+        }
+
+        static void ReadScript (BinaryReader reader)
+        {
+            for (ushort command = reader.ReadUInt16(); true; command = reader.ReadUInt16())
+            {
+                // Console.Write(GetCommandName(command));
+
+                // TODO: Support arguments
+
+                if (command == 0x0002)
+                    break;
+            }
+
+            Console.WriteLine("Script ended. Returning...");
             return;
         }
     }
