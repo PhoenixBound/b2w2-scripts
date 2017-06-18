e931b9681da712aa3dc8ac17cb5b7b85654593b9
PhoenixBound <phoenixbound.github@gmail.com>
2017-06-18 01:02:32 -0500

    Begin work on XML stuff Lots of it is untested. Lots of it I just learned in the last hour. That's a bad combination...

--- gen5_parse_compile/ScriptCommand.cs
+++ gen5_parse_compile/ScriptCommand.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
 
 namespace gen5_parse_compile
 {
@@ -104,6 +108,58 @@ namespace gen5_parse_compile
                 paramsSize += c.GetParamSize();
             }
             return paramsSize;
+        }
+
+        private string GetXmlCommandName(ushort id, ref bool canUseXml)
+        {
+            string cmdName = null;
+
+            if (!File.Exists("cmd_table.xml"))
+            {
+                Console.WriteLine("Command table not found. Generic command names must be used.");
+                canUseXml = false;
+                return cmdName;
+            }
+
+            if (!IsCommandTableValid("cmd_table.xml"))
+            {
+                Console.WriteLine("Command table failed to validate, forcing generic names.");
+                canUseXml = false;
+                return cmdName;
+            }
+
+            FileStream cmdTable = File.Open("cmd_table.xml", FileMode.Open, FileAccess.Read);
+
+            using (XmlReader cmdTableReader = XmlReader.Create(cmdTable))
+            {
+                cmdTableReader.MoveToContent();
+                if (!cmdTableReader.ReadToDescendant("command"))
+                {
+                    Console.WriteLine("That...isn't a good XML file, I think.");
+                    Console.WriteLine("Please add <command> elements to your XML file.");
+                    return cmdName;
+                }
+            }
+
+            return cmdName;
+        }
+
+        private bool IsCommandTableValid(string filename)
+        {
+            bool ret = true;
+
+            XmlSchemaSet schemas = new XmlSchemaSet();
+            schemas.Add("", "cmd_table.xsd");
+
+            XDocument tableDoc = XDocument.Load(filename);
+            tableDoc.Validate(schemas, (o, e) =>
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Can't use the command table, see above and the XML schema.");
+                ret = false;
+            });
+
+            return ret;
         }
     }
 }
